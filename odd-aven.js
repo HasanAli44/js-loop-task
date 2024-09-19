03a76922368c06da46aa7f8e8e3afab7b9f309ae
HasanAli44 <mdhasanali8439@gmail.com>
2024-09-19 19:33:32 +0800

    js loop add=even number

---
+++ odd-aven.js
@@ -0,0 +1,11 @@
+for (let num = 60; num < 100; num++) {
+  if (num % 2 === 1) {
+    console.log(num);
+  }
+}
+
+for (let num = 78; num < 98; num++) {
+  if (num % 2 === 0) {
+    console.log(num);
+  }
+}
